4b8a32173c76d02c28388d2dfa82c13bf0d96153
Emily Castillo <emilycastillox@gmail.com>
2022-05-28 21:10:11 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://emilyc:emilyc@cluster0.uuqhr.mongodb.net/user?retryWrites=true&w=majority', 
+    'dbName': 'login1'
+};
---
+++ public/main.js
@@ -0,0 +1,70 @@
+const fire = document.getElementsByClassName("fa-fire");
+const trash = document.getElementsByClassName("fa-trash");
+const bookmark = document.getElementsByClassName('fa-bookmark')
+
+Array.from(fire).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const likes = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'likes':likes,
+            'action': 'like'
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+Array.from(bookmark).forEach(function(element) {
+  element.addEventListener('click', function(e){
+    e.preventDefault()
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const bookmarks = e.target.classList.contains('blue') ? true : false
+    fetch('bookmarks', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'bookmarked': bookmarks
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
